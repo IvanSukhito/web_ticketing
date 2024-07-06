6d1e2edb9e846e07ee47342310479093e831dd49
jovianalthaf <mpialthaf@yahoo.co.id>
2024-07-06 23:35:41 +0700

    logic tiket stok

--- app/Http/Controllers/AcaraUserController.php
+++ app/Http/Controllers/AcaraUserController.php
@@ -27,7 +27,10 @@ class AcaraUserController extends Controller
     {
         $acara = Acara::where('slug', $slug)->firstOrFail();
         // TEMP STORE TO SESSION
+
         $request > session()->put('acara_id', $acara);
+
+
         return view('Frontend.checkout', compact('acara'));
     }
 
@@ -48,16 +51,27 @@ class AcaraUserController extends Controller
 
         // ambil acara id 
         $acara = $request->session()->get('acara_id');
+
         // dd($acara); 
         // SELECT * FROM tickets WHERE acara_id = [acara_id] LIMIT 1;
+
+        // yang terjual 1 dan tiket quantitynya ada 5 berarti 5-1 = 4, maka tersisa tiketnya 4 
+        //  5 > 4 = true, 
+        // if ($ticket->quantity > $remainingTickets) {
+        //     return back()->withErrors(['quantity' => 'Jumlah tiket yang diminta melebihi stok yang tersedia.']);
+        // }
+
+
+
+
+
         $ticket = Ticket::where('acara_id', $acara->id)->first();
         // Hitung jumlah tiket yang telah terjual,dari table transactions dicari tiap acara_id dan menjumlah kuantitas dari acara id tersebut
         $totalSoldTickets = Transaction::where('acara_id', $acara->id)->sum('kuantitas');
         // Cek apakah kuantitas yang diminta melebihi jumlah tiket yang tersedia, 
-        $remainingTickets = $ticket->quantity - $totalSoldTickets;
-        if ($request->quantity > $remainingTickets) {
-            return back()->withErrors(['quantity' => 'Tiket sudah habis ']);
-        }
+
+        // 5-3 = 2 (remaining tiket)
+        $remainingTickets = $ticket->kuantitas - $totalSoldTickets;
 
         if (!$ticket) {
             return response()->json(['error' => 'Ticket not found for the selected event.'], 404);
@@ -74,20 +88,37 @@ class AcaraUserController extends Controller
         if ($validateQty + $request->quantity > $ticket->max_buy) {
             return back()->withErrors(['quantity' => 'Jumlah tiket melebihi batas maksimal pembelian.']);
         }
+        // 5(ticket->quantity)5-1 = 4 (remaining ticket)
+        // 3 (req->quantity), 5-3= 2 (remaining ticket)
+        if ($request->quantity >  $remainingTickets) {
+            return back()->withErrors(['quantity' => 'Tiket yang tersedia hanya ' . $remainingTickets . ' tiket.']);
+        } elseif ($request->quantity <= $remainingTickets) {
+            $transaction = Transaction::create([
+                'acara_id' => $acara->id,
+                'user_id' => auth()->user()->id, // pastikan user sudah login
+                'name' => $request->name,
+                'phone_number' => $request->phone_number,
+                'address' => $request->address,
+                'email' => $request->email,
+                'kuantitas' => $request->quantity,
+                'total_price' => $total_price,
+                'payment_method' => $request->payment,
+                'status' => 'Unpaid',
+            ]);
+        }
 
-
-        $transaction = Transaction::create([
-            'acara_id' => $acara->id,
-            'user_id' => auth()->user()->id, // pastikan user sudah login
-            'name' => $request->name,
-            'phone_number' => $request->phone_number,
-            'address' => $request->address,
-            'email' => $request->email,
-            'kuantitas' => $request->quantity,
-            'total_price' => $total_price,
-            'payment_method' => $request->payment,
-            'status' => 'Unpaid',
-        ]);
+        // $transaction = Transaction::create([
+        //     'acara_id' => $acara->id,
+        //     'user_id' => auth()->user()->id, // pastikan user sudah login
+        //     'name' => $request->name,
+        //     'phone_number' => $request->phone_number,
+        //     'address' => $request->address,
+        //     'email' => $request->email,
+        //     'kuantitas' => $request->quantity,
+        //     'total_price' => $total_price,
+        //     'payment_method' => $request->payment,
+        //     'status' => 'Unpaid',
+        // ]);
 
         // Debug data transaksi
         // $getTransaction = [
