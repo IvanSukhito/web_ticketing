848153dba3814b0c5557c6727c38d93a59bf326a
Ivan Sukhito <sukhitoo.ivan@gmail.com>
2024-09-05 16:52:13 +0700

    update

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -199,69 +199,13 @@
                                 <div class="col-md-6 col-lg-6 col-xl-3">
                                     <div class="footer-item">
                                         <h4 class="text-white mb-4">Useful Links</h4>
-                                        <a href="#"><i class="fas fa-angle-right me-2"></i> About Us</a>
-                                        <a href="#"><i class="fas fa-angle-right me-2"></i> Features</a>
-                                        <a href="#"><i class="fas fa-angle-right me-2"></i> Services</a>
-                                        <a href="#"><i class="fas fa-angle-right me-2"></i> FAQ's</a>
-                                        <a href="#"><i class="fas fa-angle-right me-2"></i> Blogs</a>
-                                        <a href="#"><i class="fas fa-angle-right me-2"></i> Contact</a>
+                                        <a href="#"><i class="fas fa-angle-right me-2"></i> Banner</a>
+                                        <a href="#"><i class="fas fa-angle-right me-2"></i> Recently Events</a>
+                                        <a href="#"><i class="fas fa-angle-right me-2"></i> Coming Soon Events</a>
+                                       
                                     </div>
                                 </div>
-                                <div class="col-md-6 col-lg-6 col-xl-4">
-                                    <div class="footer-item">
-                                        <h4 class="mb-4 text-white">Instagram</h4>
-                                        <div class="row g-3">
-                                            <div class="col-4">
-                                                <div class="footer-instagram rounded">
-                                                    <img src="img/instagram-footer-1.jpg" class="img-fluid w-100" alt="">
-                                                    <div class="footer-search-icon">
-                                                        <a href="img/instagram-footer-1.jpg" data-lightbox="footerInstagram-1" class="my-auto"><i class="fas fa-link text-white"></i></a>
-                                                    </div>
-                                                </div>
-                                           </div>
-                                           <div class="col-4">
-                                                <div class="footer-instagram rounded">
-                                                    <img src="img/instagram-footer-2.jpg" class="img-fluid w-100" alt="">
-                                                    <div class="footer-search-icon">
-                                                        <a href="img/instagram-footer-2.jpg" data-lightbox="footerInstagram-2" class="my-auto"><i class="fas fa-link text-white"></i></a>
-                                                    </div>
-                                                </div>
-                                           </div>
-                                            <div class="col-4">
-                                                <div class="footer-instagram rounded">
-                                                    <img src="img/instagram-footer-3.jpg" class="img-fluid w-100" alt="">
-                                                    <div class="footer-search-icon">
-                                                        <a href="img/instagram-footer-3.jpg" data-lightbox="footerInstagram-3" class="my-auto"><i class="fas fa-link text-white"></i></a>
-                                                    </div>
-                                                </div>
-                                           </div>
-                                            <div class="col-4">
-                                                <div class="footer-instagram rounded">
-                                                    <img src="img/instagram-footer-4.jpg" class="img-fluid w-100" alt="">
-                                                    <div class="footer-search-icon">
-                                                        <a href="img/instagram-footer-4.jpg" data-lightbox="footerInstagram-4" class="my-auto"><i class="fas fa-link text-white"></i></a>
-                                                    </div>
-                                                </div>
-                                           </div>
-                                            <div class="col-4">
-                                                <div class="footer-instagram rounded">
-                                                    <img src="img/instagram-footer-5.jpg" class="img-fluid w-100" alt="">
-                                                    <div class="footer-search-icon">
-                                                        <a href="img/instagram-footer-5.jpg" data-lightbox="footerInstagram-5" class="my-auto"><i class="fas fa-link text-white"></i></a>
-                                                    </div>
-                                                </div>
-                                           </div>
-                                           <div class="col-4">
-                                                <div class="footer-instagram rounded">
-                                                    <img src="img/instagram-footer-6.jpg" class="img-fluid w-100" alt="">
-                                                    <div class="footer-search-icon">
-                                                        <a href="img/instagram-footer-6.jpg" data-lightbox="footerInstagram-6" class="my-auto"><i class="fas fa-link text-white"></i></a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+ 
                             </div>
                         </div>
                         <div class="pt-5" style="border-top: 1px solid rgba(255, 255, 255, 0.08);">
@@ -275,7 +219,8 @@
                                                 </div>
                                                 <div>
                                                     <h4 class="text-white">Address</h4>
-                                                    <p class="mb-0">123 Street New York.USA</p>
+                                                    <p class="mb-0">Jl. Jalur Sutera Bar. No.Kav. 21, RT.001/RW.004, Panunggangan, Kec. Pinang, Kota Tangerang, Banten 15143
+                                                    </p>
                                                 </div>
                                             </div>
                                         </div>
@@ -286,7 +231,7 @@
                                                 </div>
                                                 <div>
                                                     <h4 class="text-white">Mail Us</h4>
-                                                    <p class="mb-0">info@example.com</p>
+                                                    <p class="mb-0">ticoz@lapaktiket.com</p>
                                                 </div>
                                             </div>
                                         </div>
@@ -297,7 +242,7 @@
                                                 </div>
                                                 <div>
                                                     <h4 class="text-white">Telephone</h4>
-                                                    <p class="mb-0">(+012) 3456 7890</p>
+                                                    <p class="mb-0">(+62) 821-2245-7443</p>
                                                 </div>
                                             </div>
                                         </div>
@@ -309,8 +254,8 @@
                     
                     <div class="col-xl-3">
                         <div class="footer-item">
-                            <h4 class="text-white mb-4">Newsletter</h4>
-                            <p class="text-white mb-3">Dolor amet sit justo amet elitr clita ipsum elitr est.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <h4 class="text-white mb-4">Notification</h4>
+                            <p class="text-white mb-3">Berlangganan <span class="text-warning">notifikasi</span> untuk mendapatkan update seputar event yang tidak kalah seru bersama <span class="text-danger">LapakTiket.com</span>.</p>
                             <div class="position-relative rounded-pill mb-4">
                                 <input class="form-control rounded-pill w-100 py-3 ps-4 pe-5" type="text" placeholder="Enter your email">
                                 <button type="button" class="btn btn-primary rounded-pill position-absolute top-0 end-0 py-2 mt-2 me-2">SignUp</button>
@@ -318,7 +263,7 @@
                             </div>
                             <div class="d-flex flex-shrink-0">
                                 <div class="footer-btn">
-                                    <a href="#" class="btn btn-lg-square rounded-circle position-relative wow tada" data-wow-delay=".9s">
+                                    <a href="https://wa.me/6282122457443" class="btn btn-lg-square rounded-circle position-relative wow tada" data-wow-delay=".9s">
                                         <i class="fa fa-phone-alt fa-2x"></i>
                                         <div class="position-absolute" style="top: 2px; right: 12px;">
                                             <span><i class="fa fa-comment-dots text-secondary"></i></span>
@@ -326,8 +271,8 @@
                                     </a>
                                 </div>
                                 <div class="d-flex flex-column ms-3 flex-shrink-0">
-                                    <span>Call to Our Experts</span>
-                                    <a href="tel:+ 0123 456 7890"><span class="text-white">Contact Us</span></a>
+                                    <span>For More Info</span>
+                                    <a href="https://wa.me/6282122457443"><span class="text-white">Contact Via WhatsApp</span></a>
                                 </div>
                             </div>
                         </div>
@@ -339,11 +284,11 @@
         
         <!-- Copyright Start -->
         <div class="container-fluid copyright py-4">
-            <div class="container">
+            <div class="container" style="justify-content: center;">
                 <div class="row g-4 align-items-center">
-                    <div class="col-md-6 text-center text-md-end mb-md-0">
-                        <span class="text-body"><a href="#" class="border-bottom text-white"><i class="fas fa-copyright text-light me-2"></i>Ticoz 2024</a>, Company, Indonesia</span>
-                    </div>
+                    
+                        <span class="text-body"><a href="#" class="border-bottom text-white"><i class="fas fa-copyright text-light me-2"></i>Ticoz 2024</a></span>
+                  
                 </div>
             </div>
         </div>
